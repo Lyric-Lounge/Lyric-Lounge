221be173d940019eff29036fcf21909586acee14
JesseDelatorre <jdelatorre051990@gmail.com>
2025-03-12 12:56:39 -0400

    Update friends-list.cjs

--- db/friends-list.cjs
+++ db/friends-list.cjs
@@ -1,10 +1,10 @@
 const client = require('./client.cjs');
 
-const createFriendsList = async (userId, friednId) => {
+const createFriendsList = async (userId, friendId) => {
   try {
     await client.query(`
       INSERT INTO friends_list (users_id, friend_id)
-      VALUES (${userId}, ${friednId});
+      VALUES (${userId}, ${friendId});
     `);
   } catch (err) {
     console.log(err);
