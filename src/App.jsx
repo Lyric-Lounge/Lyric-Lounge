b8efd6ee270a4e681e9e084f5c75220a84029667
Alex Bouthillet <alex.bouthillet@gmail.com>
2025-03-18 13:17:50 -0400

    add packages and modify profile,jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './components/home';
 import Register from './components/register.jsx';
 import NavBar from './components/navbar.jsx';
 import SplashScreen from './components/SplashPage.jsx';
-import Profile from './components/Profile.jsx';
+import Profile from './components/profile.jsx';
 import ProtectedRoute from './components/ProtectedRoutes.jsx';
 // import Player from './components/Player.jsx';
 
