6a90abb9c0f6ae78cd0bd323caec9c622fb7d3fe
Trembloc <tremblay34487@gmail.com>
2025-03-20 12:58:09 -0400

    hard coded data to the homepage for presentation

--- src/components/abby.jsx
+++ src/components/abby.jsx
@@ -1,59 +1,6 @@
 import { Box } from "@mui/material"
 import VariantButtonGroup from "./VariantButton.jsx"
-
-
-const Friendlist = () => {
-
-  return (
-
-    <section>
-      <Box component="section" sx={{ p: 2, border: '1px solid #0094c6', width: 200 }}>
-        <h1>Joey</h1>
-        <ul class="mdc-list mdc-list--two-line">
-          <li class="mdc-list-item" tabindex="0">
-            <span class="mdc-list-item__ripple"></span>
-            <span class="mdc-list-item__text">
-              <span class="mdc-list-item__primary-text">Purple Rain</span>
-              <span class="mdc-list-item__secondary-text">Prince</span>
-            </span>
-          </li>
-          <li class="mdc-list-item">
-            <span class="mdc-list-item__ripple"></span>
-            <span class="mdc-list-item__text">
-              <span class="mdc-list-item__primary-text">Let's Go Crazy</span>
-              <span class="mdc-list-item__secondary-text">Prince</span>
-            </span>
-          </li>
-          <li class="mdc-list-item">
-            <span class="mdc-list-item__ripple"></span>
-            <span class="mdc-list-item__text">
-              <span class="mdc-list-item__primary-text">Another Brick in the Wall</span>
-              <span class="mdc-list-item__secondary-text">Pink Floyd</span>
-            </span>
-          </li>
-          <li class="mdc-list-item">
-            <span class="mdc-list-item__ripple"></span>
-            <span class="mdc-list-item__text">
-              <span class="mdc-list-item__primary-text">Another One Bites the Dust</span>
-              <span class="mdc-list-item__secondary-text">Queen</span>
-            </span>
-          </li>
-          <li class="mdc-list-item">
-            <span class="mdc-list-item__ripple"></span>
-            <span class="mdc-list-item__text">
-              <span class="mdc-list-item__primary-text">We Will Rock You</span>
-              <span class="mdc-list-item__secondary-text">Queen</span>
-            </span>
-          </li>
-        </ul>
-        <VariantButtonGroup />
-      </Box>
-
-    </section>
-  )
-}
-
-const Friendlist3 = () => {
+const Friendlistabby = () => {
 
   return (
 
@@ -105,4 +52,4 @@ const Friendlist3 = () => {
     </section>
   )
 }
-export default Friendlist;
+export default Friendlistabby;
