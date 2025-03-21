f9394e3340a5db4686cc288fff95a16ad9902574
Trembloc <tremblay34487@gmail.com>
2025-03-21 11:25:33 -0400

    added css to the hardcoded profile info

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Button from '@mui/joy/Button';
+import { Box } from "@mui/material"
+
 
 
 const Profile = () => {
@@ -24,7 +26,7 @@ const Profile = () => {
   };
 
   return (
-    <section>
+    <section className="profilePage">
       <h2>Profile</h2>
       <h3>test123</h3>
       {/* Song Selector */}
@@ -35,45 +37,95 @@ const Profile = () => {
             <option key={index} value={song}>{song}</option>
           ))}
         </select>
-     <Button
-             variant="contained"
-             type="submit"
-             size="xsm"
-             sx={{
-               marginTop: 2,
-               backgroundColor: '#0094c6', 
-               color: '#000000',
-               '&:hover': {
-                 backgroundColor: '#349dfe', 
-               },
-             }}
-             onClick={addTopSong}
-          >
-            Add to Top Songs
-          </Button>
+        <Button
+          variant="contained"
+          type="submit"
+          size="xsm"
+          sx={{
+            marginTop: 2,
+            backgroundColor: '#0094c6',
+            color: '#000000',
+            '&:hover': {
+              backgroundColor: '#349dfe',
+            },
+          }}
+          onClick={addTopSong}
+        >
+          Add to Top Songs
+        </Button>
         {/* <button onClick={addTopSong}>Add to Top Songs</button> */}
       </article>
 
       {/* List of Top Songs */}
-      <section className="homeFriendsList">
-        <h3>Top Songs</h3>
-        <ul>
-          <li>Luther - Kendrik Lamar & SZA</li>
-          <li>Die With A Smile - Lady Gaga & Bruno Mars</li>
-          <li>Not Like Us - Kendrik Lamar</li>
-          <li>A Bar Song - Shaboozey</li>
-          <li>TV Off - Kendrik Lamar Featuring Lefty Gunplay</li>
-        </ul>
+
+      <section className="profileTopSongs">
+        <Box component="section" sx={{ p: 2, border: '1px solid #0094c6', width: 200 }}>
+          <h3>Top Songs</h3>
+          <ul class="mdc-list mdc-list--two-line">
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">Luther</span>
+                <span class="mdc-list-item__secondary-text">Kendrik Lamar & SZA</span>
+              </span>
+            </li>
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">Die With A Smile</span>
+                <span class="mdc-list-item__secondary-text">Lady Gaga & Bruno Mars</span>
+              </span>
+            </li>
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">Not Like Us</span>
+                <span class="mdc-list-item__secondary-text">Kendrik Lamar</span>
+              </span>
+            </li>
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">A Bar Song</span>
+                <span class="mdc-list-item__secondary-text">Shaboozey</span>
+              </span>
+            </li>
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">TV Off</span>
+                <span class="mdc-list-item__secondary-text">Kendrik Lamar Featuring Lefty Gunplay</span>
+              </span>
+            </li>
+          </ul>
+        </Box>
       </section>
 
       {/* Friends Section */}
-      <section>
-        <h3>Friends List</h3>
-        <ul>
-          <li>Joey</li>
-          <li>Sally</li>
-          <li>Abby</li>
-        </ul>
+      <section className="profileFriendsList">
+        <Box component="section" sx={{ p: 2, border: '1px solid #0094c6', width: 200 }}>
+          <h3>Friends List</h3>
+          <ul class="mdc-list mdc-list--two-line">
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">Joey</span>
+              </span>
+            </li>
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">Sally</span>
+              </span>
+            </li>
+            <li class="mdc-list-item" tabindex="0">
+              <span class="mdc-list-item__ripple"></span>
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">Abby</span>
+              </span>
+            </li>
+          </ul>
+        </Box>
         <input
           type="text"
           placeholder="Enter username"
@@ -81,25 +133,25 @@ const Profile = () => {
           onChange={(e) => setUsername(e.target.value)}
         />
         <Button
-             variant="contained"
-             type="submit"
-             size="xsm"
-             font-color="white"
-             sx={{
-               marginTop: 2,
-               backgroundColor: '#0094c6',
-               color: '#000000', 
-               '&:hover': {
-                 backgroundColor: '#349dfe', 
-               },
-             }}
-             onClick={addFriend}
-          >
-            Add Friend
-          </Button>
+          variant="contained"
+          type="submit"
+          size="xsm"
+          font-color="white"
+          sx={{
+            marginTop: 2,
+            backgroundColor: '#0094c6',
+            color: '#000000',
+            '&:hover': {
+              backgroundColor: '#349dfe',
+            },
+          }}
+          onClick={addFriend}
+        >
+          Add Friend
+        </Button>
       </section>
 
-    </section>
+    </section >
   );
 };
 
